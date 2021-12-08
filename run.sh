018c933a0f1512f277b140f2089e0aad4b37a442
Ting-En Liao <teliao@teliao-sa.cs.nycu.edu.tw>
2021-12-08 12:24:52 +0800

    run.sh added, auto run for simulation & visualization not included

---
+++ run.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+no_dp=0
+no_init=0
+no_sim=0
+no_vis=0
+
+preprocess()
+{
+    for arg in $@; do
+        case $arg in
+            "--without-dp")
+                no_dp=1   
+                ;;
+            "--without-init")
+                no_init=1
+                ;;
+            "--without-sim")
+                no_sim=1
+                ;;
+            "--without-vis")
+                no_vis=1
+                ;;
+        esac
+    done
+}
+
+data_process()
+{
+    if [ $no_dp -eq 1 ]; then
+        return
+    fi
+
+    echo "Running Data Processing..."
+
+    cd src/data_process/
+    source data_process.sh
+    cd ../..
+
+    wait
+}
+
+init()
+{
+    if [ $no_init -eq 1 ]; then
+        return
+    fi
+
+    echo "Running Initialization..."
+
+    cd src/initialization/
+    source init.sh
+    cd ../..
+
+    wait
+}
+
+sim()
+{
+    if [ $no_sim -eq 1 ]; then
+        return
+    fi
+
+    echo "Running Simulation..."
+
+    wait
+}
+
+vis()
+{
+    if [ $no_vis -eq 1 ]; then
+        return
+    fi
+
+    echo "Running Visualization..."
+
+    wait
+}
+
+main()
+{
+    preprocess $@
+
+    data_process
+    init
+    sim
+    vis
+}
+
+main $@
