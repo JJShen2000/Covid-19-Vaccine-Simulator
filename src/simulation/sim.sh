3c5fb91cd2c0ec612f46233132793b5942222bbd
xxoooxx345678 <xxoooxx345678@gmail.com>
2022-05-01 05:47:47 -0700

    fix sim.sh

--- src/simulation/sim.sh
+++ src/simulation/sim.sh
@@ -3,8 +3,7 @@
 no_aon=0
 no_leaky=0
 
-aon_fn="sfeirvd_aon"
-leaky_fn="sfeirvd_leaky"
+simulator="./simulator"
 
 data_dir="../../data/sim_data/"
 graph_dir="${data_dir}graph/*"
@@ -40,14 +39,12 @@ makec()
 	make clean
 }
 
-leaky()
+run()
 {
 	if [ $no_leaky -eq 1 ]; then
 		return
 	fi
 
-	echo "simulating leaky model"
-
 	for graph in ${graph_dir}; do
 		for conf in ${conf_dir}; do
 			for init_infector in ${init_infector_dir}; do
@@ -56,10 +53,12 @@ leaky()
 					ct="$(echo "$conf" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
 					it="$(echo "$init_infector" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
 					vt="$(echo "$vaccine" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
-					dump_state_fn="$(echo "${dump_dir}${leaky_fn}_${gt}_${ct}_${it}_${vt}_state.csv")"
-					dump_anal_fn="$(echo "${dump_dir}${leaky_fn}_${gt}_${ct}_${it}_${vt}_anal.csv")"
+					rs="$(echo "${dump_dir}${gt}_${ct}_${it}_${vt}_result.csv")"
 
-					./${leaky_fn} $graph $conf $init_infector $vaccine $dump_state_fn $dump_anal_fn
+					echo "simulating: " $gt $ct $it $vt
+					echo "          dest file: " $rs
+
+					./${simulator} -g $graph -p $conf -i $init_infector -v $vaccine -o $rs -s DESSIR
 					wait
 				done
 			done
@@ -67,68 +66,13 @@ leaky()
 	done
 
 }
-
-all_or_nothing()
-{
-	if [ $no_aon -eq 1 ]; then
-		return
-	fi
-
-	echo "simulating all-or-nothing model"
-
-	for graph in ${graph_dir}; do
-		for conf in ${conf_dir}; do
-			for init_infector in ${init_infector_dir}; do
-				for vaccine in ${vaccine_dir}; do
-					gt="$(echo "$graph" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
-					ct="$(echo "$conf" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
-					it="$(echo "$init_infector" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
-					vt="$(echo "$vaccine" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
-					dump_state_fn="$(echo "${dump_dir}${aon_fn}_${gt}_${ct}_${it}_${vt}_state.csv")"
-					dump_anal_fn="$(echo "${dump_dir}${aon_fn}_${gt}_${ct}_${it}_${vt}_anal.csv")"
-
-					./${aon_fn} $graph $conf $init_infector $vaccine $dump_state_fn $dump_anal_fn
-					wait
-				done
-			done
-		done
-	done
-}
-
-analysis()
-{
-	for i in $(seq 1 10);do
-	for graph in ${graph_dir}; do
-		for conf in ${conf_dir}; do
-			for init_infector in ${init_infector_dir}; do
-				for vaccine in ${vaccine_dir}; do
-					gt="$(echo "$graph" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
-					ct="$(echo "$conf" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
-					it="$(echo "$init_infector" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
-					vt="$(echo "$vaccine" | awk -F / '{print $NF}' | sed 's/.txt//; s/.conf//')"
-
-					if [ $vt != "no_vaccine" ];then
-						continue
-					fi
-					echo $i
-					./${aon_fn} $graph $conf $init_infector $vaccine /dev/null /dev/null > ../../anal/result${i}.txt
-					wait
-				done
-			done
-		done
-	done
-	done
-}
-
 
 main()
 {
 	preprocess $@
 	makef
 
-#	leaky
-	all_or_nothing
-#	analysis
+	run
 
 	makec
 }
